21a5df43aa35ab5fab6b212b2b0b71d50b9f82d5
Heitan <heitan104@gmail.com>
2024-11-09 16:07:56 -0500

    added tailwind

--- voiceMedic/src/app/index.tsx
+++ voiceMedic/src/app/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+import "../../global.css";
+
 export default function Page() {
   return (
-    <View>
+    <View className="flex items-center justify-center h-screen">
       <Text>Beginner code</Text>
     </View>
   );
